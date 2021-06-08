d9523b409303dea8f92e8bb8f40d85d1599739f0
DotsenkoArtem <52153157+DotsenkoArtem@users.noreply.github.com>
2021-06-08 11:15:53 +0500

    maxima-html modal draft for check

--- my-works/maxima/html-css/js/dist/main.dev.js
+++ my-works/maxima/html-css/js/dist/main.dev.js
@@ -17,34 +17,3 @@ function menuClose() {
   document.body.classList.remove('scroll-hidden');
   navMobOverl.classList.remove('open');
 } //------------------------------------------------------------------------------
-
-
-var modalOverlay = document.querySelector(".modal__overlay"),
-    closeButtons = document.querySelectorAll(".modal__close"),
-    openButton = document.querySelector(".modal-trigger"); // ОТКРЫТИЕ МОДАЛЬНОГО ОКНА
-
-openButton.addEventListener("click", function () {
-  openModalById('#modal');
-}); // ЗАКРЫТИЕ МОДАЛЬНОГО ОКНА
-
-for (var i = 0; i < closeButtons.length; i++) {
-  closeButtons[i].addEventListener('click', function () {
-    closeModal('#modal');
-  });
-} // ФУНКЦИЯ ОТКРЫТИЯ МОДАЛЬНОГО ОКНА ПО ID
-
-
-function openModalById(modalId) {
-  var modal = document.querySelector(modalId);
-  modal.classList.remove('closed');
-  modal.classList.add('open');
-  document.documentElement.classList.add('scroll-hidden');
-} // ФУНКЦИЯ ЗАКРЫТИЯ МОДАЛЬНОГО ОКНА ПО ID
-
-
-function closeModal(modalId) {
-  var modal = document.querySelector(modalId);
-  modal.classList.remove('open');
-  modal.classList.add('closed');
-  document.documentElement.classList.remove('scroll-hidden');
-}
