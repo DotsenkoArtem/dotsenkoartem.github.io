9145f94193ff23ff3bb3d5d8afbe3f8a2eddfc1c
Artem Dotsenko <artemdoc1@rambler.ru>
2022-02-18 12:29:51 +0500

    ИНИЦИАЛИАЦИЯ ПРОЕКТА
    - добавлена сборка проекта YS
    - добавлены шрифты, необходимые для проекта
    - больше ничего не начинал, папки и файлы еще содержат исходный мусор

---
+++ current-works/YS/src/js/script1.js
@@ -0,0 +1,34 @@
+"use strict"
+const RED_COLOR = '#ff0000';
+
+let brother = 'Алексей Александрович';
+
+setTimeout(()=> {
+    document.body.style.backgroundColor = 'rgba(50, 50, 75, .35)';
+    document.body.style.color = 'rgba(255, 255, 255, 1)';
+}, 5000);
+
+// console.log("Привет, бро!");
+
+
+// console.log("Файл script1.js: Вывод данных.");
+
+
+
+// console.log("Файл script1.js: Вывод данных.");
+
+// console.log("Файл script1.js: Вывод данных.");
+// console.log("Файл script1.js: Вывод данных.");
+// console.log("Файл script1.js: Вывод данных.");
+// console.log("Файл script1.js: Вывод данных.");
+
+
+//             console.log("Файл script1.js: Вывод данных.");
+// console.log("Файл script1.js: Вывод данных.");
+
+
+
+//             console.log("Файл script1.js: Вывод данных.");
+// console.log("Файл script1.js: Вывод данных.");
+// console.log("Файл script1.js: Вывод данных.");
+
