0bdef6c5d519c611c12a07895fc71264c6afae5e
Artem Dotsenko <artemdoc1@rambler.ru>
2022-04-11 17:00:14 +0500

    Эподключено 2 модальных окна: колбэк и аудит

--- dev/current-works/tlevel/public/js/script.js
+++ dev/current-works/tlevel/public/js/script.js
@@ -1,6 +1,43 @@
 "use strict"; // window.onload = function (){
+// MODALS
+
+var modalOpenBtns = document.querySelectorAll('.modal-trigger');
+
+if (modalOpenBtns) {
+  var _loop = function _loop(i) {
+    var modalOpenBtn = modalOpenBtns[i];
+    modalOpenBtn.addEventListener('click', function () {
+      openModal(modalOpenBtn);
+    });
+  };
+
+  for (var i = 0; i < modalOpenBtns.length; i++) {
+    _loop(i);
+  }
+} // Functions
+
+
+function openModal(modalOpenBtn) {
+  var modal = document.getElementById(modalOpenBtn.dataset.target);
+  modal.classList.remove('closed');
+  modal.classList.add('opened');
+  var modalCloseBtns = modal.getElementsByClassName('modal-close');
+
+  for (var _i = 0; _i < modalCloseBtns.length; _i++) {
+    var modalCloseBtn = modalCloseBtns[_i];
+    modalCloseBtn.addEventListener('click', function () {
+      closeModal(modal);
+    });
+  }
+}
+
+function closeModal(modal) {
+  modal.classList.remove('opened');
+  modal.classList.add('closed');
+} // ------------------------------------------------
 // SLIDERS 
 // Expert-slider
+
 
 var expertSlider = new Swiper('.expert__slider', {
   loop: true,
@@ -90,8 +127,8 @@ function initSwiper() {
     advSwiper = undefined;
     advSlideContainer.removeAttribute('style');
 
-    for (var i = 0; i < advSlides.length; i++) {
-      advSlides[i].removeAttribute('style');
+    for (var _i2 = 0; _i2 < advSlides.length; _i2++) {
+      advSlides[_i2].removeAttribute('style');
     }
   }
 } //Swiper plugin initialization
