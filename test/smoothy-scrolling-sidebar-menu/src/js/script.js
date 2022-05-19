adb056b714e701df9d79fc363a7416e2d14a8f3f
Artem Dotsenko <artemdoc1@rambler.ru>
2022-05-19 13:30:36 +0500

    Добавлен компонент плавной прокрутки двухколоночной страницы, меню - в сайдбаре - ТЕСТ

---
+++ test/smoothy-scrolling-sidebar-menu/src/js/script.js
@@ -0,0 +1,40 @@
+"use strict"
+
+let currentScroll   = 0;
+let scrollTo        = 0;
+
+/*
+currentScroll    - значение текущей прокрутки блока
+scrollTo         - значение, до которого требуется прокрутить блок
+*/
+
+$(function(){
+    // Вывод в консоль по клику на кнопку значения текущей прокрутки блока .main
+    /*
+    $('#btn').on('click', function(){
+        console.log(`currentScroll: ${currentScroll}`);
+    })
+    */
+
+    // Определение значения текущей прокрутки при скролле
+    $('.main').on('scroll', function(){
+        currentScroll = $('.main').scrollTop();
+    })
+
+    $('a[href^="#"]').on('click', function(event) {
+        // отменяем стандартное действие
+        event.preventDefault();
+
+        let target          = $(this).attr("href"),
+            targetOffset    = $(target).offset().top;
+        /*
+        * target        - в переменную заносим информацию о том, к какому блоку надо перейти
+        * targetOffset  - определяем положение блока в окне
+        */
+
+        currentScroll = $('.main').scrollTop();
+        scrollTo    = targetOffset + currentScroll;
+
+        $('.main').animate({scrollTop: scrollTo}, 500, 'swing');
+    });
+});
