472db504a2afcc39a3135234773749a39fe633dc
DotsenkoArtem <artemdoc1@rambler.ru>
2025-06-10 16:32:53 +0500

    Edited

--- widget/widgetestimation/js/common.js
+++ widget/widgetestimation/js/common.js
@@ -1,136 +1,167 @@
-window.addEventListener('load', function () {
+window.addEventListener("load", function () {
   let order;
 
+  checkStars();
 
-  checkStars()
+  $('[type="tel"]:not(.enter-code-form [type="tel"])').inputmask({
+    mask: "+7 (999) 999-9999",
+  });
 
+  handleForms();
 
-  $('[type="tel"]:not(.enter-code-form [type="tel"])').inputmask({"mask": "+7 (999) 999-9999"});
+  document.forms["contact-director-form"].addEventListener(
+    "submit",
+    function (e) {
+      e.preventDefault();
+      if (this.cdFormAgree.checked) {
+        changeScreen(this, lowRateAlert);
+      } else alert("Необходимо принять условия пользовательского соглашения.");
+    }
+  );
 
+  document.forms["share-review-form"].addEventListener("submit", function (e) {
+    e.preventDefault();
+    if (this.shrFormAgree.checked) {
+      changeScreen(this, selectLogin);
+    } else alert("Необходимо принять условия пользовательского соглашения.");
+  });
 
-
-
-
-
-
-  document.forms['contact-director-form'].addEventListener('submit', function(e) {
-    e.preventDefault()
-    if(this.cdFormAgree.checked) {
-      changeScreen (this, lowRateAlert)
-    } else alert("Необходимо принять условия пользовательского соглашения.")
-  })
-
-
-  document.forms['share-review-form'].addEventListener('submit', function(e) {
-    e.preventDefault()
-    if(this.shrFormAgree.checked) {
-      changeScreen (this, selectLogin)
-    } else alert("Необходимо принять условия пользовательского соглашения.")
-  })
-
-
-  document.forms['login-email-form'].querySelector('.login-email-form-submit').addEventListener('click', function(e) {
-    e.preventDefault()
-    changeScreen (this, enterCode)
-    // alert("Таймер")
-    setTimer(0, 10);
-  })
-
-
+  document.forms["login-email-form"]
+    .querySelector(".login-email-form-submit")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
+      changeScreen(this, enterCode);
+      // alert("Таймер")
+      setTimer(0, 10);
+    });
 
   // Code Form
-  const codeForm = document.forms['enter-code-form']
-  const codeInputs = codeForm.querySelectorAll('.enter-code-form-input')
-  codeInputs.forEach(input => {
-    input.addEventListener('input', ()=>{
-      if(
-        input.value !== '0' &&
-        input.value !== '1' &&
-        input.value !== '2' &&
-        input.value !== '3' &&
-        input.value !== '4' &&
-        input.value !== '5' &&
-        input.value !== '6' &&
-        input.value !== '7' &&
-        input.value !== '8' &&
-        input.value !== '9' 
-        ) {
-        input.value = ''
+  const codeForm = document.forms["enter-code-form"];
+  const codeInputs = codeForm.querySelectorAll(".enter-code-form-input");
+  codeInputs.forEach((input) => {
+    input.addEventListener("input", () => {
+      if (
+        input.value !== "0" &&
+        input.value !== "1" &&
+        input.value !== "2" &&
+        input.value !== "3" &&
+        input.value !== "4" &&
+        input.value !== "5" &&
+        input.value !== "6" &&
+        input.value !== "7" &&
+        input.value !== "8" &&
+        input.value !== "9"
+      ) {
+        input.value = "";
       } else {
-        if(input.closest('label').nextElementSibling) {
-          input.closest('label').nextElementSibling.querySelector('.enter-code-form-input').focus()
-        } else{
+        if (input.closest("label").nextElementSibling) {
+          input
+            .closest("label")
+            .nextElementSibling.querySelector(".enter-code-form-input")
+            .focus();
+        } else {
           // Отправить форму, затем сменить экран на #successAlert
-          changeScreen (input, successAlert)
+          changeScreen(input, successAlert);
         }
       }
-      
-    })
-  })
+    });
+  });
 
-
-  codeInputs.forEach(input => {
-    input.addEventListener('keyup', (e)=>{
-
-      if(e.code === 'Backspace') {
-        input.value = ''
-        if(input.closest('label').previousElementSibling) {
-          input.closest('label').previousElementSibling.querySelector('.enter-code-form-input').focus()
+  codeInputs.forEach((input) => {
+    input.addEventListener("keyup", (e) => {
+      if (e.code === "Backspace") {
+        input.value = "";
+        if (input.closest("label").previousElementSibling) {
+          input
+            .closest("label")
+            .previousElementSibling.querySelector(".enter-code-form-input")
+            .focus();
         }
       }
-    })
-  })
+    });
+  });
+});
 
-})
+function checkStars() {
+  const stars = document.querySelectorAll(".js-select-rate");
 
-function checkStars () {
-  const stars = document.querySelectorAll('.js-select-rate')
-  stars.forEach(elem => {
-    elem.addEventListener('click', function () {
-      order = elem.dataset.starOrder
+  const starsBox = document.querySelector(".js-select-rate-box");
 
-      for(let i = 0; i < stars.length; i++) {
-        if(stars[i].dataset.starOrder <= order) {
-          stars[i].classList.add('is-active')
+  starsBox.addEventListener("mouseleave", function () {
+    stars.forEach((star) => {
+      star.classList.remove("is-hovered");
+    });
+  });
+
+  stars.forEach((elem) => {
+    elem.addEventListener("click", function () {
+      order = elem.dataset.starOrder;
+
+      for (let i = 0; i < stars.length; i++) {
+        if (stars[i].dataset.starOrder <= order) {
+          stars[i].classList.add("is-active");
         } else {
-          stars[i].classList.remove('is-active')
+          stars[i].classList.remove("is-active");
         }
       }
 
-      let shareStars = document.querySelectorAll('.share-review-star')
-      for(let j = 0; j < order; j++) {
-        shareStars[j].classList.add('is-active')
+      let shareStars = document.querySelectorAll(".share-review-star");
+      for (let j = 0; j < order; j++) {
+        shareStars[j].classList.add("is-active");
       }
 
-
-
-      setTimeout(function() {
-        if(elem.dataset.starOrder <= 3) {
-          changeScreen(elem, contactDirector)
+      setTimeout(function () {
+        if (elem.dataset.starOrder <= 3) {
+          changeScreen(elem, contactDirector);
         } else {
-          changeScreen(elem, hignRate)
+          changeScreen(elem, hignRate);
         }
-      }, 1000)
+      }, 1000);
+    });
 
+    elem.addEventListener("mouseover", function () {
+      order = elem.dataset.starOrder;
 
-    })
-  })
+      for (let i = 0; i < stars.length; i++) {
+        if (stars[i].dataset.starOrder <= order) {
+          stars[i].classList.add("is-hovered");
+        } else {
+          stars[i].classList.remove("is-hovered");
+        }
+      }
+    });
+  });
 }
 
-function changeScreen (currentScreen, targetSreen) {
-  currentScreen.closest('.estimation-screen').classList.add('screen-hidden')
-  targetSreen.classList.remove('screen-hidden')
+function changeScreen(currentScreen, targetSreen) {
+  currentScreen.closest(".estimation-screen").classList.add("screen-hidden");
+  console.log("targetSreen: ", targetSreen);
+  targetSreen.classList.remove("screen-hidden");
 }
 
+function handleForms() {
+  Array.from(document.forms).forEach((form) => {
+    handleUploads(form);
+  });
+}
 
-
+function handleUploads(form) {
+  let inputTypeFile = form.querySelector('input[type="file"]');
+  if (inputTypeFile) {
+    inputTypeFile.addEventListener("change", function () {
+      if (this.files.length >= 1) {
+        inputTypeFile.closest(".label-type-file").innerText = `Фото добавлено`;
+        console.log("this.files: ", this.files);
+      }
+    });
+  }
+}
 
 // TIMER
 function setTimer(startMinutes, startSeconds) {
-  localStorage.clear()
+  localStorage.clear();
   // Высчитали время таймера
-  let timerStartValue =
-    (startMinutes * 60 + startSeconds) * 1000;
+  let timerStartValue = (startMinutes * 60 + startSeconds) * 1000;
 
   // let timerTmpStartValue = parseInt(
   //   window.localStorage.getItem("timerTmpStartValue")
@@ -167,9 +198,11 @@ function setTimer(startMinutes, startSeconds) {
       // timerStopStamp = currentTime
 
       clearTimeout(timerId);
-      document.querySelector('.js-code-timeout-btn').removeAttribute('disabled')
+      document
+        .querySelector(".js-code-timeout-btn")
+        .removeAttribute("disabled");
 
-      document.querySelector('.js-code-timeout-row').setAttribute('hidden', '')
+      document.querySelector(".js-code-timeout-row").setAttribute("hidden", "");
     } else {
       // Текущий таймстамп-остаток таймера
       let timerCurrentValue = timerStopStamp - currentTime;
@@ -183,20 +216,16 @@ function setTimer(startMinutes, startSeconds) {
       minutes.innerHTML = `${setZero(timerCurrentMinutes)}`;
       seconds.innerHTML = `${setZero(timerCurrentSeconds)}`;
 
-      document.querySelector('.js-code-timeout-row').removeAttribute('hidden', '')
+      document
+        .querySelector(".js-code-timeout-row")
+        .removeAttribute("hidden", "");
 
       // Запись в LocalStorage
       window.localStorage.setItem("timerEnd", timerStopStamp);
 
       timerId = setTimeout(updateTimer, 1000);
     }
-
-
   }, 0);
-
-
-
-   
 
   // }
 
@@ -204,22 +233,17 @@ function setTimer(startMinutes, startSeconds) {
     return val < 10 ? `0${val}` : `${val}`;
   }
 
-
-
   // updateTimer();
 }
-document.querySelector('.js-code-timeout-btn').addEventListener('click', function() {
-    
-  this.setAttribute('disabled', '')
-  // alert("Таймер по кнопке")
-  setTimer(0, 10);
-})
+document
+  .querySelector(".js-code-timeout-btn")
+  .addEventListener("click", function () {
+    this.setAttribute("disabled", "");
+    // alert("Таймер по кнопке")
+    setTimer(0, 10);
+  });
 
-
-
-
-
-window.addEventListener('load', ()=>{
+window.addEventListener("load", () => {
   // document.querySelectorAll('.estimation-screen').forEach(screen => screen.style.display = `none`)
   // enterCode.style.display = `block`
-})
+});
